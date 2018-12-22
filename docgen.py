27969d68a3fc12d716d67d278d622bd80f615fcc
Ryan Harrison <mail@ryanharrison.co.uk>
2018-12-22 18:30:17 +0000

    Generate all docs from directory keeping structure

--- docgen.py
+++ docgen.py
@@ -1,6 +1,11 @@
+import os
 from markdown2 import Markdown
 
 TEMPLATE_DIR = "template"
+OUTPUT_DIR = "docs"
+RAW_DIR = "raw"
+
+markdowner = Markdown(extras=["tables", "fenced-code-blocks"])
 
 
 def read_base_template():
@@ -17,32 +22,46 @@ def create_styles():
     return styles
 
 
-def build_doc_page(base, styles, content):
-    page = base.replace("{{ styles }}", styles)
-    page = page.replace("{{ content }}", content)
-    return page
+# TODO: Replace <title> with content filename
+def build_doc_page(base, styles, markdown):
+    withStyles = base.replace("{{ styles }}", styles)
+    content = markdowner.convert(markdown)
+    withContent = withStyles.replace("{{ content }}", content)
+    return withContent
 
 
-def convert_markdown(markdown):
-    markdowner = Markdown(extras=["tables", "fenced-code-blocks"])
-    html = markdowner.convert(markdown)
-    return html
+def walk_raw_docs():
+    docs = []
+    for (root, _, filenames) in os.walk(RAW_DIR):
+        for filename in filenames:
+
+            with open(os.path.join(root, filename), 'r') as doc_file:
+                markdown = doc_file.read().strip()
+
+            dirpath = os.path.relpath(root, RAW_DIR).replace(".", "")
+            name = os.path.splitext(filename)[0].replace(" ", "-").lower()
+            docs.append({"path": dirpath, "name": name, "markdown": markdown})
+    return docs
 
 
-def write_page(path, html):
-    with open(path, 'w') as content_file:
-        content_file.write(html)
-
-
-if __name__ == "__main__":
+def generate_docs(raw_docs):
     base = read_base_template()
     styles = create_styles()
 
-    with open("docs.md", 'r') as content_file:
-        markdown = content_file.read().strip()
+    for doc in raw_docs:
+        page = build_doc_page(base, styles, doc["markdown"])
+        doc_dir_path = os.path.join(OUTPUT_DIR, doc["path"])
+        doc_path = os.path.join(doc_dir_path, f"{doc['name']}.html")
 
-    doc_content = convert_markdown(markdown)
+        if not os.path.exists(doc_dir_path):
+            os.makedirs(doc_dir_path)
 
-    page = build_doc_page(base, styles, doc_content)
+        with open(doc_path, 'w') as output_file:
+            output_file.write(page)
 
-    write_page("docs.html", page)
+
+if __name__ == "__main__":
+
+    raw_docs = walk_raw_docs()
+
+    generate_docs(raw_docs)
